f865c7cfe199f7aae838d0d5014b1005ce0e91cb
fer1su <rafael.rodrigues022@hotmail.com>
2023-04-28 20:57:36 -0300

    atividades

--- index.js
+++ index.js
@@ -22,8 +22,35 @@
 // console.log(receita.toLowerCase().includes("cenoura"))
 // console.log(receita.includes("abc"))
 
-const frase = prompt("Escreva uma frase")
-console.log(frase.toUpperCase())
-const frase2 = frase.replaceall("o","i")
-console.log(frase2)
-console.log(frase.length())
+// const frase = prompt("Escreva uma frase")
+// console.log(frase.toUpperCase())
+// const frase2 = frase.replaceall("o","i")
+// console.log(frase2)
+// console.log(frase.length())
+
+// const numeros = ["a", "b", "c", "d", "e"]
+
+// console.log(numeros.length);
+// console.log(numeros.includes("a"));
+// numeros.push(prompt("Digite ume letra"))
+// console.log(numeros);
+// numeros.pop()
+// console.log(numeros)
+
+// numeros.splice(1,2)
+// console.log(numeros)
+
+// const letra = Number(prompt("Digite um número de 0 a 4"))
+// console.log(numeros[letra])
+
+// numeros[letra] === undefined ?
+// console.log("Você deve informar um numero entre 0 e 4"):
+// console.log(numeros[letra]);
+
+const numeros = [1, 2, 3, 4, 5, 6]
+console.log(numeros.length);
+numeros.push(7)
+console.log(numeros)
+numeros.splice(3,2)
+console.log(numeros)
+console.log(numeros.length)
