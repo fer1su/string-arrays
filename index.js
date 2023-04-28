578c7daa85539d141c69a363283f00a195f69b15
fer1su <rafael.rodrigues022@hotmail.com>
2023-04-27 21:30:26 -0300

    arrays

---
+++ index.js
@@ -0,0 +1,29 @@
+// const nome = prompt("insira o nome")
+// console.log(`o "${nome}" é turista`)
+
+// const nome = prompt("insira seu nome")
+// const cor = prompt ("insira sua cor favorita")
+// console.log(`A cor favorita do ${nome} é ${cor}`)
+
+// const nome = prompt("Digite seu nome")
+// console.log(nome.length)
+// nome.length === 0 ? console.log("Você precisa inserir seu nome"):console.log("Seu nome é",nome)
+
+// const nome = prompt("Insira seu nome")
+// console.log(nome.toLowerCase())
+// console.log(nome.toUpperCase())
+
+// const nome = "       a"
+// console.log(nome.trim())
+
+// const receita = "abacaxi"
+// const receita2 = receita.replace("abacaxi","cenoura")
+// console.log(receita2)
+// console.log(receita.toLowerCase().includes("cenoura"))
+// console.log(receita.includes("abc"))
+
+const frase = prompt("Escreva uma frase")
+console.log(frase.toUpperCase())
+const frase2 = frase.replaceall("o","i")
+console.log(frase2)
+console.log(frase.length())
